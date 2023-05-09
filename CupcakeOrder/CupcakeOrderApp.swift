e3c7f7175a7add7a6e44081815a700839a43181d
Tatiana Bondarenko <62190724+bondarenkotatiana96@users.noreply.github.com>
2023-05-08 21:54:33 -0700

    Add files via upload

---
+++ CupcakeOrder/CupcakeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeOrderApp.swift
+//  CupcakeOrder
+//
+//  Created by Tatiana Bondarenko on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
